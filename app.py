e1b2c766ba6eb44490e62e5ba52237bc9b676aae
rafaelpardor <rafaelpardor@protonmail.com>
2022-07-10 22:28:23 -0500

    App

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def index():
-    return "Test"
+def home_view():
+        return "<h1>Welcome to Geeks for Geeks</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
