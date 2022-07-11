ff1e9dc9b56498618242750455a331496089c530
rafaelpardor <rafaelpardor@protonmail.com>
2022-07-11 00:18:28 -0500

    App

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import url_for
+from flask import request
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static',
+            template_folder='template')
 
 
 @app.route("/")
 def home_view():
-        return "<h1>Welcome Amdocss</h1>"
+        return render_template("index.html")
+
+@app.route("/login", methods=['POST'])
+def redirect_form():
+    data = request.form
+    print(data)
+    if data["username"] != "rafa" and data["passwd"] != "rafa":
+        print(data["username"], data["passwd"])
+        return "Error"
+    return redirect(url_for("dashboard"))
+
+@app.route("/dashboard", methods=['GET'])
+def dashboard():
+        return render_template("dashboard.html")
+
 
 # if __name__ == "__main__":
 #     app.run(debug=True, host='0.0.0.0')
